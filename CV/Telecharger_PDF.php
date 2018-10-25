d78c70a1727f70d170768cf7f658d3d6b5a5f551
Omar Bouaouina <35614626+OmarBouaouina@users.noreply.github.com>
2018-10-25 20:45:25 +0100

    First Website Upload

---
+++ CV/Telecharger_PDF.php
@@ -0,0 +1,23 @@
+<?php 
+
+// include autoloader
+require_once 'dompdf/autoload.inc.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml(file_get_contents('CV.html'));
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper(array(0,0,800,1065), 'portrait');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream("Testing");
+
+
+?>
